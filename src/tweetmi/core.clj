9dd76b3dd453bd496ff7bd7de5ae19eac75728f5
Boaz Rosenan <brosenan@gmail.com>
2018-01-17 08:41:36 +0200

    software-evolution

--- src/tweetmi/core.clj
+++ src/tweetmi/core.clj
@@ -6,17 +6,41 @@
  (def msec-in-day (* 1000 60 60 24))
 
  ;; Rules should reside in their own source file
- (clg/defrule followee-tweets [[user day] author tweet ts]
+ (defn ts-to-day [ts]
+   (quot ts msec-in-day))
+
+ (clg/defrule followee-tweets [[user day] author text ts]
    [:tweetmi/follows user author] (clg/by user)
-   [:tweetmi/tweeted author tweet ts restricted] (clg/by author)
-   (let [day (quot ts msec-in-day)]))
+   [:tweetmi/tweeted author text ts attrs] (clg/by author)
+   (let [day (ts-to-day ts)]))
+
+ (defn days-in-range [from-day to-day]
+   (let [day-range (range from-day to-day)]
+     (if (> (count day-range) 20)
+       []
+       day-range)))
 
  (clg/defclause tl-1
-   [:tweetmi/timeline user from-day to-day -> author tweet ts]
-   (let [day-range (range from-day to-day)])
-   (when-not (> (count day-range) 20))
-   (for [day day-range])
-   [followee-tweets [user day] author tweet ts])
+   [:tweetmi/timeline user from-day to-day -> author text ts]
+   (for [day (days-in-range from-day to-day)])
+   [followee-tweets [user day] author text ts])
+
+ (clg/defclause tl-2
+   [:tweetmi/timeline user from-day to-day -> user text ts]
+   [:tweetmi/tweeted user text ts attrs] (clg/by user)
+   (for [day (days-in-range from-day to-day)])
+   (when (= (ts-to-day ts) day)))
+
+ (clg/defrule tweet-by-mention [[user day] author text ts]
+   [:tweetmi/tweeted author text ts attrs] (clg/by author)
+   (let [day (ts-to-day ts)])
+   (for [user-handle (re-seq #"@[a-zA-Z0-9]+" text)])
+   (let [user (subs user-handle 1)]))
+
+ (clg/defclause tl-3
+   [:tweetmi/timeline user from-day to-day -> author text ts]
+   (for [day (days-in-range from-day to-day)])
+   [tweet-by-mention [user day] author text ts])
 
  (clg/defrule follower [user f]
    [:tweetmi/follows f user] (clg/by f))
--- test/tweetmi/core_test.clj
+++ test/tweetmi/core_test.clj
@@ -7,7 +7,7 @@
  (let [ts (+ (* 3 msec-in-day) 1234)]
    (scenario
     (as "bob"
-        (emit [:tweetmi/tweeted "bob" "hello" ts false]))
+        (emit [:tweetmi/tweeted "bob" "hello" ts {}]))
     (as "alice"
         (emit [:tweetmi/follows "alice" "bob"]))
     (apply-rules [:tweetmi.core/followee-tweets ["alice" 3]]))
@@ -16,15 +16,25 @@
 (fact
  (scenario
   (as "bob"
-      (emit [:tweetmi/tweeted "bob" "Sunday" (* 1 msec-in-day) false])
-      (emit [:tweetmi/tweeted "bob" "Monday" (* 2 msec-in-day) false])
-      (emit [:tweetmi/tweeted "bob" "Tuesday" (* 3 msec-in-day) false])
-      (emit [:tweetmi/tweeted "bob" "Wednesday" (* 4 msec-in-day) false]))
+      (emit [:tweetmi/tweeted "bob" "Sunday" (* 1 msec-in-day) {}])
+      (emit [:tweetmi/tweeted "bob" "Monday" (* 2 msec-in-day) {}])
+      (emit [:tweetmi/tweeted "bob" "Tuesday" (* 3 msec-in-day) {}])
+      (emit [:tweetmi/tweeted "bob" "Wednesday" (* 4 msec-in-day) {}]))
+  (as "charlie"
+      (emit [:tweetmi/tweeted "charlie" "This is for @alice" (* 2 msec-in-day) {}])
+      (emit [:tweetmi/tweeted "charlie" "This is not for alice" (* 2 msec-in-day) {}]))
   (as "alice"
+      (emit [:tweetmi/tweeted "alice" "sunday" (* 1 msec-in-day) {}])
+      (emit [:tweetmi/tweeted "alice" "monday" (* 2 msec-in-day) {}])
+      (emit [:tweetmi/tweeted "alice" "tuesday" (* 3 msec-in-day) {}])
+      (emit [:tweetmi/tweeted "alice" "wednesday" (* 4 msec-in-day) {}])
       (emit [:tweetmi/follows "alice" "bob"])
       (query [:tweetmi/timeline "alice" 2 4])
       => #{["bob" "Monday" (* 2 msec-in-day)]
-           ["bob" "Tuesday" (* 3 msec-in-day)]}
+           ["bob" "Tuesday" (* 3 msec-in-day)]
+           ["alice" "monday" (* 2 msec-in-day)]
+           ["alice" "tuesday" (* 3 msec-in-day)]
+           ["charlie" "This is for @alice" (* 2 msec-in-day)]}
       ;; The query should protect itself, and not answer queries for more than 20 days at a time
       (query [:tweetmi/timeline "alice" 2 23]) => map?)))
 
